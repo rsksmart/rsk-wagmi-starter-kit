4dda167dabbcb599a69cc9172d66e026da1da58c
chrisarevalodev <christian.arevalo@ext.rootstocklabs.com>
2024-05-12 23:29:40 -0500

    📱 Worked on responsive

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,14 +11,18 @@ export default function Home() {
   };
 
   return (
-    <main className="h-[300vh] max-w-[1100px] mx-auto">
+    <main className="mb-[200px] max-w-[1100px] mx-auto">
       <section className="mx-auto flex flex-col items-center justify-center min-h-[90vh]">
-        <h1 className="text-[7em] text-center font-bold font-neueMachinaBold text-balance leading-tight text-black">
-          <span className="bg-orange-400 pt-5 px-2 ">Hackathon's</span>{" "}
-          <span className="bg-fuchsia-500 pt-5 px-2">Starter</span>{" "}
-          <span className="bg-fuchsia-500 pt-5 px-2">Kits</span>
+        <h1 className="text-[3em] md:text-[4em] lg:text-[7em] flex flex-col gap-5 text-center font-bold font-neueMachinaBold text-balance md:leading-[auto] lg:leading-tight text-black">
+          <span className="bg-orange-400 lg:pt-5 px-2 leading-tight">
+            Hackathon's
+          </span>{" "}
+          <span>
+            <span className="bg-fuchsia-500 pt-5 px-2">Starter</span>{" "}
+            <span className="bg-fuchsia-500 pt-5 px-2">Kits</span>
+          </span>
         </h1>
-        <p className="text-[2em] text-center font-bold">
+        <p className="text-[1.5em] md:text-[2em] text-center font-bold">
           An introduction for building smart contracts on Bitcoin
         </p>
         <div className="flex gap-5">
