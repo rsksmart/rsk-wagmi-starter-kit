2ef66176467671ada131c0bb4bf83e9947ddf11a
chrisarevalodev <christian.arevalo@ext.rootstocklabs.com>
2024-05-20 13:01:10 -0500

    🚧 Fixed vite bug and first steps in aa integration

--- src/components/etherspot/Demo.tsx
+++ src/components/etherspot/Demo.tsx
@@ -1,145 +1,205 @@
-import {
-  CardTitle,
-  CardDescription,
-  CardHeader,
-  CardContent,
-  Card,
-} from "@/components/ui/card";
-import Button from "@/components/ui/button";
-import { useState } from "react";
-import Loader from "@/components/ui/loader";
-import { formatAddress } from "@/lib/utils";
-import { Check, Copy } from "lucide-react";
-import { useToast } from "@/components/ui/use-toast";
+// import {
+//   CardTitle,
+//   CardDescription,
+//   CardHeader,
+//   CardContent,
+//   Card,
+// } from "@/components/ui/card";
+// import Button from "@/components/ui/button";
+// import { useEffect, useState } from "react";
+// import Loader from "@/components/ui/loader";
+// // import { PrimeSdk, EtherspotBundler } from "@etherspot/prime-sdk";
+// import { formatAddress } from "@/lib/utils";
+// import { Check, Copy } from "lucide-react";
+// import { useToast } from "@/components/ui/use-toast";
+// import { generatePrivateKey, privateKeyToAccount } from "viem/accounts";
 
 export default function Demo() {
-  const { toast } = useToast();
-  const [loading, setLoading] = useState<boolean>(false);
-  const [copied, setCopied] = useState<boolean>(false);
-  const [address, setAddress] = useState<string>("");
+  // const [sdk, setSdk] = useState<PrimeSdk | null>(null);
+  // const { toast } = useToast();
+  // const [loading, setLoading] = useState<boolean>(false);
+  // const [eoaWalletAddress, setEoaWalletAddress] = useState("");
+  // const [etherspotWalletAddress, setEtherspotWalletAddress] = useState("");
+  // const [copied, setCopied] = useState<boolean>(false);
+  // const [eoaPrivateKey, setEoaPrivateKey] = useState("");
 
-  const generatePaymaster = async () => {
-    setLoading(true);
-    try {
-      setTimeout(() => {
-        setAddress(() => "0x0000000000000000000000000000000000");
-        setLoading(false);
-      }, 2000);
-    } catch (e) {
-      console.error(e);
-      setLoading(false);
-    }
-  };
+  // // const generatePaymaster = async () => {
+  // //   if (!sdk) return;
+  // //   setLoading(true);
 
-  const copy = (address: string) => {
-    navigator.clipboard.writeText(address);
-    setCopied(true);
-    setTimeout(() => setCopied(false), 3000);
-  };
+  // //   try {
+  // //     const address = await sdk.getCounterFactualAddress();
+  // //     setEtherspotWalletAddress(address);
+  // //     console.log("EtherspotWallet address:", address);
+  // //   } catch (error) {
+  // //     console.error("Error generating Etherspot Wallet:", error);
+  // //   } finally {
+  // //     setLoading(false);
+  // //   }
+  // // };
 
-  const estimateCost = () => {
-    toast({
-      title: "Estimate cost",
-      description: "Estimate cost",
-    });
-  };
+  // const copy = (address: string) => {
+  //   navigator.clipboard.writeText(address);
+  //   setCopied(true);
+  //   setTimeout(() => setCopied(false), 3000);
+  // };
 
-  const sendTx = () => {
-    toast({
-      title: "Send transaction",
-      description: "Send transaction",
-    });
-  };
+  // const estimateCost = () => {
+  //   toast({
+  //     title: "Estimate cost",
+  //     description: "Estimate cost",
+  //   });
+  // };
+
+  // const sendTx = () => {
+  //   toast({
+  //     title: "Send transaction",
+  //     description: "Send transaction",
+  //   });
+  // };
+
+  // const generateRandomEOA = async () => {
+  //   const pk = generatePrivateKey();
+  //   const account = privateKeyToAccount(pk);
+  //   setEoaWalletAddress(account.address);
+  //   setEoaPrivateKey(pk);
+  // };
+
+  // useEffect(() => {
+  //   if (eoaPrivateKey) {
+  //     const bundlerApiKey =
+  //       "eyJvcmciOiI2NTIzZjY5MzUwOTBmNzAwMDFiYjJkZWIiLCJpZCI6IjMxMDZiOGY2NTRhZTRhZTM4MGVjYjJiN2Q2NDMzMjM4IiwiaCI6Im11cm11cjEyOCJ9";
+  //     const customBundlerUrl = "https://rootstocktestnet-bundler.etherspot.io/";
+
+  //     // const primeSdk = new PrimeSdk(
+  //     //   { privateKey: eoaPrivateKey },
+  //     //   {
+  //     //     chainId: 31,
+  //     //     bundlerProvider: new EtherspotBundler(
+  //     //       31,
+  //     //       bundlerApiKey,
+  //     //       customBundlerUrl
+  //     //     ),
+  //     //   }
+  //     // );
+
+  //     // setSdk(primeSdk);
+  //   }
+  // }, [eoaPrivateKey]);
 
   return (
-    <Card className="w-full max-w-md mx-auto">
-      <CardHeader className="space-y-2">
-        <CardTitle className="text-2xl font-bold">
-          Account Abstraction
-        </CardTitle>
-        <CardDescription className="text-gray-500 dark:text-gray-400">
-          Empower your users to seamlessly interact with your decentralized
-          application without the hassle of managing private keys.
-        </CardDescription>
-      </CardHeader>
-      <CardContent className="space-y-4">
-        <div className="flex items-center gap-4">
-          <span className="font-neueMachinaBold px-2 pt-[5px] text-black rounded-full bg-fuchsia-500 grid place-items-center">
-            01
-          </span>
-          <p>Generate the paymaster address</p>
-        </div>
-        <div className="flex flex-col justify-center">
-          <Button
-            className="justify-self-center"
-            onClick={generatePaymaster}
-            disabled={loading}
-          >
-            {loading ? <Loader /> : "Generate"}
-          </Button>
-          {address && (
-            <p className="mt-3 opacity-60 flex gap-3">
-              Wallet: {formatAddress(address)}{" "}
-              <button onClick={() => copy(address)}>
-                {copied ? <Check /> : <Copy />}
-              </button>
-            </p>
-          )}
-        </div>
-        <div className="flex items-center gap-4">
-          <span className="font-neueMachinaBold px-2 pt-[5px] text-black rounded-full bg-orange-500 grid place-items-center">
-            02
-          </span>
-          <p>Estimate the transaction cost:</p>
-        </div>
-        <div className="flex flex-col justify-center">
-          <Button
-            className="justify-self-center"
-            onClick={estimateCost}
-            disabled={loading || !address}
-          >
-            {loading ? <Loader /> : "Estimate"}
-          </Button>
-          {/* {
-            TODO: show estimated cost
-            cost &&
-            <p className="mt-2 opacity-60">Tx cost:  {cost}</p>
-          } */}
-        </div>
-        <div className="flex items-center gap-4">
-          <span className="font-neueMachinaBold px-2 pt-[5px] text-black rounded-full bg-green-500 grid place-items-center">
-            03
-          </span>
-          <p>Send the transaction</p>
-        </div>
-        <div className="flex flex-col justify-center">
-          <Button
-            className="justify-self-center"
-            onClick={sendTx}
-            disabled={loading || !address}
-          >
-            {loading ? <Loader /> : "Send"}
-          </Button>
-        </div>
-        <div className="bg-secondary p-4 rounded-lg">
-          <h4 className="text-lg font-medium mb-2">Account Balance</h4>
-          <div className="flex items-center justify-between">
-            <span className="text-gray-400">rBTC Tokens</span>
-            <span className="font-medium">
-              {address ? (
-                loading ? (
-                  <Loader />
-                ) : (
-                  //   TODO: show balance
-                  0
-                )
-              ) : (
-                0
-              )}
-            </span>
-          </div>
-        </div>
-      </CardContent>
-    </Card>
+    <h1>Demo</h1>
+    // <Card className="w-full max-w-md mx-auto">
+    //   <CardHeader className="space-y-2">
+    //     <CardTitle className="text-2xl font-bold">
+    //       Account Abstraction
+    //     </CardTitle>
+    //     <CardDescription className="text-gray-500 dark:text-gray-400">
+    //       Empower your users to seamlessly interact with your decentralized
+    //       application without the hassle of managing private keys.
+    //     </CardDescription>
+    //   </CardHeader>
+    //   <CardContent className="space-y-4">
+    //     <div className="flex items-center gap-4">
+    //       <span className="font-neueMachinaBold px-2 pt-[5px] text-black rounded-full bg-fuchsia-500 grid place-items-center">
+    //         01
+    //       </span>
+    //       <p>Generate a random wallet</p>
+    //     </div>
+    //     <div className="flex flex-col justify-center">
+    //       <Button
+    //         className="justify-self-center"
+    //         onClick={generateRandomEOA}
+    //         disabled={loading}
+    //       >
+    //         {loading ? <Loader /> : "Generate"}
+    //       </Button>
+    //       {eoaWalletAddress && (
+    //         <p className="mt-3 opacity-60 flex gap-3">
+    //           Wallet: {formatAddress(eoaWalletAddress)}{" "}
+    //           <button onClick={() => copy(eoaWalletAddress)}>
+    //             {copied ? <Check /> : <Copy />}
+    //           </button>
+    //         </p>
+    //       )}
+    //     </div>
+    //     <div className="flex items-center gap-4">
+    //       <span className="font-neueMachinaBold px-2 pt-[5px] text-black rounded-full bg-fuchsia-500 grid place-items-center">
+    //         02
+    //       </span>
+    //       <p>Generate the paymaster address</p>
+    //     </div>
+    //     <div className="flex flex-col justify-center">
+    //       <Button
+    //         className="justify-self-center"
+    //         // onClick={}
+    //         disabled={loading}
+    //       >
+    //         {loading ? <Loader /> : "Generate"}
+    //       </Button>
+    //       {etherspotWalletAddress && (
+    //         <p className="mt-3 opacity-60 flex gap-3">
+    //           Wallet: {formatAddress(etherspotWalletAddress)}{" "}
+    //           <button onClick={() => copy(etherspotWalletAddress)}>
+    //             {copied ? <Check /> : <Copy />}
+    //           </button>
+    //         </p>
+    //       )}
+    //     </div>
+    //     <div className="flex items-center gap-4">
+    //       <span className="font-neueMachinaBold px-2 pt-[5px] text-black rounded-full bg-orange-500 grid place-items-center">
+    //         03
+    //       </span>
+    //       <p>Estimate the transaction cost:</p>
+    //     </div>
+    //     <div className="flex flex-col justify-center">
+    //       <Button
+    //         className="justify-self-center"
+    //         onClick={estimateCost}
+    //         disabled={loading || !etherspotWalletAddress}
+    //       >
+    //         {loading ? <Loader /> : "Estimate"}
+    //       </Button>
+    //       {/* {
+    //         TODO: show estimated cost
+    //         cost &&
+    //         <p className="mt-2 opacity-60">Tx cost:  {cost}</p>
+    //       } */}
+    //     </div>
+    //     <div className="flex items-center gap-4">
+    //       <span className="font-neueMachinaBold px-2 pt-[5px] text-black rounded-full bg-green-500 grid place-items-center">
+    //         04
+    //       </span>
+    //       <p>Send the transaction</p>
+    //     </div>
+    //     <div className="flex flex-col justify-center">
+    //       <Button
+    //         className="justify-self-center"
+    //         onClick={sendTx}
+    //         disabled={loading || !etherspotWalletAddress}
+    //       >
+    //         {loading ? <Loader /> : "Send"}
+    //       </Button>
+    //     </div>
+    //     <div className="bg-secondary p-4 rounded-lg">
+    //       <h4 className="text-lg font-medium mb-2">Account Balance</h4>
+    //       <div className="flex items-center justify-between">
+    //         <span className="text-gray-400">rBTC Tokens</span>
+    //         <span className="font-medium">
+    //           {etherspotWalletAddress ? (
+    //             loading ? (
+    //               <Loader />
+    //             ) : (
+    //               //   TODO: show balance
+    //               0
+    //             )
+    //           ) : (
+    //             0
+    //           )}
+    //         </span>
+    //       </div>
+    //     </div>
+    //   </CardContent>
+    // </Card>
   );
 }
